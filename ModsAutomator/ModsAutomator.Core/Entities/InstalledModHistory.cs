9ad8f5fee066eb5da0807d635075b4dddb949a69
Adham Abdelhameed Elsharkawy <adham.a.sharkawy@gmail.com>
2026-02-02 08:23:27 +0200

    adding history mod and abstracting Mod

---
+++ ModsAutomator/ModsAutomator.Core/Entities/InstalledModHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsAutomator.Core.Entities
+{
+    public class InstalledModHistory
+    {
+        public Guid Id { get; init; }
+
+        public Guid ModId { get; init; }
+
+        public string Version { get; init; } = "";
+
+        public string AppVersion { get; init; } = "";
+
+        public DateOnly InstalledAt { get; set; }
+
+        public DateOnly? RemovedAt { get; set; }
+
+        public string? LocalFilePath { get; set; } = "";
+
+        public bool IsRollbackTarget { get; set; }
+    }
+}
